2a97f43225bbbc71a33dda74b1431f70fb548929
Javier Molina <j.sheerp@gmail.com>
2016-10-06 00:00:54 +0200

    C11Test

---
+++ src/test/java/javier/molina/SVC/miw/upm/es/C11Test.java
@@ -0,0 +1,25 @@
+package javier.molina.SVC.miw.upm.es;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C11Test {
+
+	private C11 c11;
+    
+    @Before
+    public void before(){
+        c11 = new C11();
+    }  
+    @Test
+    public void testM1() {
+        assertEquals("m1", this.c11.m1());
+    }
+
+    @Test
+    public void testM2() {
+        assertEquals("m2", this.c11.m2());
+    }
+}
